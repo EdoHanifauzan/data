71491d4f7b7e70899294120e0633c5ff7496d581
EdoHanifauzan <111823951+EdoHanifauzan@users.noreply.github.com>
2022-11-15 06:39:58 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+mkdir -p ~/.streamlit/
+echo "\[general]\n\
+email =\" edo.hanifauzan.satria@yahoo.com\"\n\
+" >
+echo "\
+[server]\n\\headless=true\n\
+enablesCORS=false\n\
+prot=$PORT\n\
+"> ~.streamlit/config.toml
