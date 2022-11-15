8cfff9a843540ec0a777868bea94891bda83bade
EdoHanifauzan <111823951+EdoHanifauzan@users.noreply.github.com>
2022-11-15 07:33:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ echo "\[server]\n\
 email =\" edo.hanifauzan.satria@yahoo.com\"\n\
 " >
 echo "\
-[server]\n\\headless=true\n\
+[server]\n\
+headless=true\n\
 enablesCORS=false\n\
 port=$PORT\n\
 "> ~.streamlit/config.toml
