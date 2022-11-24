7cddd3c0c28c5ff5d870f8d33cbcec239d8cc23c
EdoHanifauzan <111823951+EdoHanifauzan@users.noreply.github.com>
2022-11-24 10:45:18 +0700

    Add files via upload

--- machine_learning.py
+++ machine_learning.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd 
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import StackingClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from sklearn.neighbors import KneighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.decomposition import PCA
 
 
@@ -28,33 +32,29 @@ st.sidebar.markdown("""
 
 nama_dataset= st.sidebar.selectbox(
     'pilih Dataset',
-    ('BUnga Iris','Kanker Payudara','Digit Angka','student_data')
+    ('Kanker Payudara','Bunga Iris','student_data','Digit Angka')
 )
+df=pd.read_csv('https://raw.githubusercontent.com/EdoHanifauzan/data/Dataset/student_data.csv')
 
 st.write(f"## Dataset {nama_dataset}")
-
 algoritma= st.sidebar.selectbox(
 	'pilih algoritma',
-	('KNN',"SVM","Random Forest")
+	("SVM","KNN","Random Forest","Decision Tree")
 )
-
 def pilih_dataset(nama):
 	data=None
-	if nama =='BUnga Iris':
+	if nama =='Bunga Iris':
 		data=datasets.load_iris()
 	elif nama =='Kanker Payudara':
 		data=datasets.load_breast_cancer()
 	elif nama =='Digit Angka':
 		data=datasets.load_digits()
-	elif nama =='Dataset user':
-		data= pd.read_csv('https://raw.githubusercontent.com/EdoHanifauzan/data/Dataset/student_data.csv')
+	else:
+		data= df.to_numpy()
 	X =data.data
 	y =data.target
 	return X ,y
 
-if data is not None:
-	st.write("dataset :")
-	st.write(data)
 
 X, y = pilih_dataset(nama_dataset)
 st.write('Jumlah Baris dan Kolom :',X.shape)
@@ -68,25 +68,30 @@ def tambah_parameter(nama_algoritma):
 	elif nama_algoritma =='SVM':
 		C=st.sidebar.slider('C',0.01,20.0)
 		params['C']=C
+	elif nama_algoritma =='Decision Tree':
+		max_depth=st.sidebar.slider('max_depth',1,20)
+		params['max_depth']=max_depth
 	elif nama_algoritma =='Random Forest':
 		max_depth=st.sidebar.slider('max_depth',1,20)
 		params['max_depth']=max_depth
 		n_estimators=st.sidebar.slider('n_estimators',1,100)
-		params['n_estimators']=n_estimator
+		params['n_estimators']=n_estimators
 	return params
 params=tambah_parameter(algoritma)
 
 def pilih_klasifikasi(nama_algoritma, params):
 	algo=None
 	if nama_algoritma=='KNN':
-		algo=KneighborsClassifier(n_neighbors=params['K'])
+		algo=KNeighborsClassifier(n_neighbors=params['K'])
 	elif nama_algoritma=='SVM':
-		algo=SVCr(Cs=params['C'])
-	elif nama_algoritma=='KNN':
+		algo=SVC(C=params['C'])
+	elif nama_algoritma =='Decision Tree':
+		algo= DecisionTreeClassifier(max_depth=params['max_depth'],)
+	elif nama_algoritma =='Random Forest':
 		algo=RandomForestClassifier(n_estimators=params['n_estimators'],max_depth=params['max_depth'], random_state=4321)
 	return algo
 
-algo=pilih_klasifikasi(algoritma, parsams)
+algo=pilih_klasifikasi(algoritma, params)
 
 ### proses skalifikasi###
 X_train, X_test, y_train, y_test=train_test_split(X, y, test_size= 0.25, random_state=4321)
@@ -95,21 +100,16 @@ algo.fit(X_train,y_train)
 y_pred=algo.predict(X_test)
 
 acc=accuracy_score(y_test, y_pred)
-
 st.write(f'algoritma = {algoritma}')
-st.write(f'Akurasi  =', a)
+st.write(f'Akurasi  =', acc)
 
 # proyeksi data PCA #
-#pca= PCA(2)
-#X_projected = pca.fit_transform(X)
-#X1= X_projected[:,0]
-#x2= X_projected[:,1]
+pca= PCA(2)
+X_projected = pca.fit_transform(X)
+X1= X_projected[:,0]
+X2= X_projected[:,1]
 
-#fig= plt.figure()
-#plt.scatter(x1, x2, c=y, alpha=0.75, cmap='viridis')
+fig= plt.figure()
+plt.scatter(X1, X2, c=y, alpha=0.75, cmap='viridis')
 
-#plt.xlabel('principal Component 1')
-#plt.ylabel('principal Component 2')
-#plt.colorbar
-
-#st.pyplot(fig)	
+st.pyplot(fig)	
